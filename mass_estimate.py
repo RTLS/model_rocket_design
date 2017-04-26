cf42b221071aea67764d088d4f57ee8032b91847
RTLS <carter.e.pedersen@gmail.com>
2017-04-26 08:27:13 -0700

    Also these files

--- mass_estimate.py
+++ mass_estimate.py
@@ -1,61 +1,20 @@
 import math
 
 import numpy as np
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 import pdb
 
+from trajectory import trajectory
 
-def drag(u,Cd,A):
-  rho = 1.225    #kg    #FIXME
-  return .5*rho*u*np.absolute(u)*Cd*A
-
-#Simulation Parameters
-n = 1001    #simulation steps
-t = np.linspace(0,150,num=n)    #time vector
-dt = t[1] - t[0]
-
-
-# Motor Parameters
-g407_t = np.array([0, 0.024, 0.057, 0.252, 0.500, 0.765, 1.000, 1.250, 1.502, 1.751, 1.999, 2.121, 2.300])
-g407_thrust = np.array([0, 74.325, 67.005, 65.879, 63.063, 60.248, 54.054, 47.298, 36.599, 25.338, 12.951, 3.941, 0])
-thrust = [np.interp(t[i], g407_t, g407_thrust) if t[i] < g407_t[-1] else 0 for i in xrange(len(t))]    #motor thrust over time
-g407_t = np.array([0, g407_t[-1]+7])
-g407_m = np.array([.1234, .0607])
-g407_m = [np.interp(t[i], g407_t, g407_m) for i in xrange(len(t))]    #motor mass over time
-
-#Rocket Parameters
-dry_mass = np.linspace(.75,2,num=9)    # kg
-mass = np.array([[elem + dry_mass[i] for elem in g407_m] for i in xrange(len(dry_mass))])
-Cd = [.75 if t[i] < g407_t[-1] else 1.5 for i in xrange(len(t))]
-A = [0.0042 if t[i] < g407_t[-1] else 1.65 for i in xrange(len(t))]   # m^2
-
-# Launch Parameters
-flight_angle = 2/180.0*math.pi    #rads
-transform = np.array([math.sin(flight_angle), math.cos(flight_angle)])
-g = 9.81;
 
 altitudes = []
-#pdb.set_trace()
-for j in xrange(len(mass[:,0])):
-  position = np.zeros([n,2], dtype=np.float)    #position vector, 2 dimensional
-  velocity = np.zeros([n,2], dtype=np.float)    #velocity vector, 2 dimensional
-  accel = np.zeros([n,2], dtype=np.float)    #acceleration vector, 2 dimensional
-  for i in xrange(len(t)-1):
-    #pdb.set_trace()
-    # If we're on the ground (either before launch or after landing)
-    if position[i,1] < 0:
-      pass
-    # Modified Euler method.
-    else:
-      m1 = (transform*thrust[i] - transform*drag(velocity[i,:], Cd[i], A[i]))/mass[j,i] - np.array([0,g])
-      m2 = (transform*thrust[i+1] - transform*drag(velocity[i,:]+dt*m1, Cd[i+1], A[i+1]))/mass[j,i+1] - np.array([0,g])
-      accel[i, :] = (m1+m2)/2.0
-      velocity[i+1, :] = velocity[i,:] + dt*accel[i, :] 
-      position[i+1, :] = position[i,:] + dt*(velocity[i,:] + velocity[i+1,:])/2.0
+masses = np.linspace(.2,.5,10)
 
-  pdb.set_trace()
-  altitudes.append(max(position[:, 1]))
+for mass in masses:
+  t, position, velocity, accel, thrust = trajectory(mass)
+  altitudes.append(max(position[:,1]))
 
-pdb.set_trace()
-""" Plotting """
-
+print(max(altitudes))
+plt.figure(1)
+plt.plot(masses, altitudes)
+plt.show()
--- trajectory.py
+++ trajectory.py
@@ -1,82 +1,87 @@
 import math
 
 import numpy as np
-import matplotlib.pyplot as plt
 import pdb
 
 
-def drag(u,Cd,A):
-  rho = 1.225    #kg    #FIXME
-  return .5*rho*u*np.absolute(u)*Cd*A
+def drag(height,u,Cd,A):
+  height = 3.28084*height
+  T = 59 - 0.00356 * height    # Temp in Farenheighteit
+  p = 473.1 * np.exp(1.73 - 0.000048 * height)    # Pressure
+  rho = p / (1718 * (T + 459.7))    # State equation
+  rho = 515.379 * rho    # Converting to kg/m^3
+  drag = .5*rho*u*np.absolute(u)*Cd*A
+  return drag
 
-#Simulation Parameters
-n = 250001    #simulation steps
-t = np.linspace(0,150,num=n)    #time vector
-dt = t[1] - t[0]
-position = np.zeros([n,2], dtype=np.float)    #position vector, 2 dimensional
-velocity = np.zeros([n,2], dtype=np.float)    #velocity vector, 2 dimensional
-accel = np.zeros([n,2], dtype=np.float)    #acceleration vector, 2 dimensional
+def trajectory(m=None, delta_t=None, angle=None):
+  """ Simulates a launch trajectory"""
 
-# Motor Parameters
-g407_t = np.array([0, 0.024, 0.057, 0.252, 0.500, 0.765, 1.000, 1.250, 1.502, 1.751, 1.999, 2.121, 2.300])
-g407_thrust = np.array([0, 74.325, 67.005, 65.879, 63.063, 60.248, 54.054, 47.298, 36.599, 25.338, 12.951, 3.941, 0])
-thrust = [np.interp(t[i], g407_t, g407_thrust) if t[i] < g407_t[-1] else 0 for i in xrange(len(t))]    #motor thrust over time
-g407_t = np.array([0, g407_t[-1]+7])
-g407_m = np.array([.1234, .0607])
-g407_m = [np.interp(t[i], g407_t, g407_m) for i in xrange(len(t))]    #motor mass over time
-
-#Rocket Parameters
-dry_mass = .75    # kg
-mass = [elem + dry_mass for elem in g407_m]
-Cd = [.75 if t[i] < g407_t[-1] else 1.5 for i in xrange(len(t))]
-A = [0.0042 if t[i] < g407_t[-1] else 1.65 for i in xrange(len(t))]   # m^2
-
-# Launch Parameters
-flight_angle = 2/180.0*math.pi    #rads
-transform = np.array([math.sin(flight_angle), math.cos(flight_angle)])
-g = 9.81;
-
-for i in xrange(len(t)-1):
-  #pdb.set_trace()
-  # If we're on the ground (either before launch or after landing)
-  if position[i,1] < 0:
-    pass
-  # Modified Euler method.
+  #Simulation Parameters
+  if delta_t is None:
+    dt = .01    #simulation steps
   else:
-    m1 = (transform*thrust[i] - transform*drag(velocity[i,:], Cd[i], A[i]))/mass[i] - np.array([0,g])
-    m2 = (transform*thrust[i+1] - transform*drag(velocity[i,:]+dt*m1, Cd[i+1], A[i+1]))/mass[i+1] - np.array([0,g])
-    accel[i, :] = (m1+m2)/2.0
-    velocity[i+1, :] = velocity[i,:] + dt*accel[i, :] 
-    position[i+1, :] = position[i,:] + dt*(velocity[i,:] + velocity[i+1,:])/2.0
+    dt = delta_t
 
-""" Plotting """
-# Altitude
-plt.figure(1)
-plt.plot(t, position[:,1])
-plt.ylabel('Height [m]')
-plt.xlabel('Time [s]')
-plt.title('Altitude')
-plt.show()
+  t = np.array([0])    #time vector
+  position = np.zeros([1,2], dtype=np.float)    #position vector, 2 dimensional
+  velocity = np.zeros([1,2], dtype=np.float)    #velocity vector, 2 dimensional
+  accel = np.zeros([1,2], dtype=np.float)    #acceleration vector, 2 dimensional
 
-# Velocity
-plt.figure(2)
-plt.plot(t, velocity[:,1])
-plt.ylabel('Velocity [m/s]')
-plt.xlabel('Time [s]')
-plt.title('Velocity')
-plt.show()
+  # Motor Parameters
+  g407_t = np.array([0, 0.024, 0.057, 0.252, 0.500, 0.765, 1.000, 1.250, 1.502, 1.751, 1.999, 2.121, 2.300, 9.70])
+  g407_thrust = np.array([0, 74.325, 67.005, 65.879, 63.063, 60.248, 54.054, 47.298, 36.599, 25.338, 12.951, 3.941, 0, 0])
+  thrust = [np.interp(i*dt, g407_t, g407_thrust)  for i in xrange(int(g407_t[-1]/dt)+4)]    #motor thrust over time
+  g407_t = np.array([0, g407_t[-1]])
+  g407_m = np.array([.1234, .0607])
+  g407_m = [np.interp(i*dt, g407_t, g407_m) for i in xrange(int(g407_t[-1]/dt)+4)]    #motor mass over time
 
-# Acceleration
-plt.figure(3)
-plt.subplot(211)
-plt.plot(t, accel[:,1])
-plt.ylabel('Acceleration m/s^2')
-plt.xlabel('Time [s]')
-plt.title('Acceleration')
-plt.subplot(212)
-plt.plot(t, thrust)
-plt.ylabel('Thrust [N]')
-plt.xlabel('Time [s]')
-plt.title('Thrust')
-plt.show()
+  #Rocket Parameters
+  if m is None:
+    dry_mass = .4
+  else:
+    dry_mass = m    # kg
+  mass = [elem + dry_mass for elem in g407_m]
+  Cd1 = .75    # pre parachute deployment
+  Cd2 = 1.5    # post parachute deployment
+  A1 = .0042   # pre parachute deployment
+  A2 = 1.65    # post parachute deploymen
 
+  # Launch Parameters
+  if angle is None:
+    flight_angle = 2/180.0*math.pi    #rads
+  else:
+    flight_angle = angle/180.0*math.pi
+  transform = np.array([math.sin(flight_angle), math.cos(flight_angle)])
+  g = 9.81;
+
+  landed = False
+  burnout = False
+  i = 0
+  #pdb.set_trace()
+  while not landed:
+    # If we're on the ground (either before launch or after landing)
+    if not burnout:
+      if t[-1] > g407_t[-1]:
+        burnout = True
+      if position[i,1] < 0:  # helps gets rid of weird startup dynamics
+        pass
+      else:
+        drag1 = drag(position[i,1], velocity[i,:], Cd1, A1)
+        m1 = (transform*thrust[i] - drag1)/mass[i] - np.array([0,g])
+        drag2 = drag(position[i,1], velocity[i,:]+dt*m1, Cd1, A1)
+        m2 = (transform*thrust[i+1] - drag2)/mass[i+1] - np.array([0,g])
+    else: # If we're burned out
+      drag1 = drag(position[i,1], velocity[i,:], Cd2, A2)
+      m1 = -1*drag1/dry_mass - np.array([0,g])
+      drag2 = drag(position[i,1], velocity[i,:]+dt*m1, Cd2, A2)
+      m2 = -1*drag2/dry_mass - np.array([0,g])
+      if position[-1,1] <= 0:
+        landed = True
+
+    accel = np.append(accel, np.array([(m1+m2)/2.0]), axis=0)
+    velocity = np.append(velocity, np.array([velocity[i,:] + dt*(m1+m2)/2.0]), axis=0) 
+    position = np.append(position, np.array([position[i,:] + dt*(velocity[i,:] + velocity[i+1,:])/2.0]), axis=0)
+    i+=1
+    t = np.append(t, np.array([t[-1]+dt]))
+   
+  return t, position, velocity, accel, thrust
